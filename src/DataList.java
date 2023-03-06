353c4f1568d7360c25f972fb021237ebaa11e415
Victor Hüni <victor.hueni@protonmail.com>
2023-03-06 19:21:51 +0100

    isof implements Collection interface, extends ArrayLsit

--- src/DataList.java
+++ src/DataList.java
@@ -1,92 +1,21 @@
 import java.util.*;
 
-public class DataList<T extends Valuable<?>> implements Collection<T> {
-    private List<T> data;
-
-    public DataList() {
-        this.data = new ArrayList<>();
-    }
+public class DataList<T extends Valuable<?>> extends ArrayList<T> {
 
     public double getTotal(){
         double total = 0;
-        for (T t:data) {
+        for (T t:this) {
             total += t.getValue().doubleValue();
         }
         return total;
     }
 
     public double getAverage(){
-        if(data.isEmpty()){
+        if(this.isEmpty()){
             return 0;
         }
-        return this.getTotal() / data.size();
+        return this.getTotal() / this.size();
     }
 
-
-
-    @Override
-    public int size() {
-        return data.size();
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return data.isEmpty();
-    }
-
-    @Override
-    public boolean contains(Object o) {
-        return data.contains(o);
-    }
-
-    @Override
-    public Iterator<T> iterator() {
-        return data.iterator();
-    }
-
-    @Override
-    public Object[] toArray() {
-        return data.toArray();
-    }
-
-    @Override
-    public <T1> T1[] toArray(T1[] a) {
-        return data.toArray(a);
-    }
-
-    @Override
-    public boolean add(T t) {
-        return data.add(t);
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        return data.remove(o);
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return data.containsAll(c);
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends T> c) {
-        return data.addAll(c);
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        return data.removeAll(c);
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        return data.retainAll(c);
-    }
-
-    @Override
-    public void clear() {
-        data.clear();
-    }
 }
 
